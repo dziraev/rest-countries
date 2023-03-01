a414f6f0b94980fd413c92126e69b5cdfbc0c91b
Dmitry Dziraev <dziraevdmitry@gmail.com>
2023-03-01 18:00:40 +0300

    Finished Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,43 @@ import { Container } from './Container'
 
 
 const HeaderEl = styled.div`
-	box-shadow: var(--shadow);
-	background-color: var(--colors-ui-base);
+  box-shadow: var(--shadow);
+  background-color: var(--colors-ui-base);
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 
 const Title = styled.a.attrs({
 	href: '/',
-})``;
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
 
-const ThemeSwitcher = styled.div``
+const ThemeSwitcher = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  text-transform: capitalize;
+
+`;
 
 export const Header = () => {
 	const [theme, setTheme] = useState('light');
 
 	const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
 
-	useEffect(()=>{
+	useEffect(() => {
 		document.body.setAttribute('data-theme', theme)
 	}, [theme])
 
@@ -35,7 +54,8 @@ export const Header = () => {
 						Where is the world?
 					</Title>
 					<ThemeSwitcher onClick={toggleTheme}>
-						<IoMoon /> Light Theme
+						{theme === 'light' ? <IoMoonOutline size='14px' /> : <IoMoon size='14px'/>}
+						<span>{theme} Theme</span>
 					</ThemeSwitcher>
 				</Wrapper>
 			</Container>
