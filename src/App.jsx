fdb17e801eefc99bde8e48af132bf925c0c73333
Dmitry Dziraev <dziraevdmitry@gmail.com>
2023-03-02 16:55:58 +0300

    Added Search Bar and Ð Custom Select

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Controls } from "./components/Controls";
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Controls />
+      </Main>
+    </>
+  );
+}
+
+export default App;
