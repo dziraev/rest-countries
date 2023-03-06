f0f419c3d66fd894639dfe20a31b904a2d2f0ac0
Dmitry Dziraev <dziraevdmitry@gmail.com>
2023-03-06 15:54:06 +0300

    Info component is done

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import { useNavigate, useParams } from "react-router-dom";
+import {IoArrowBack} from 'react-icons/io5'
+import { searchByCountry } from "../config";
+import { Info } from "../components/Info";
+import { Button } from "../components/Button";
+
 
 export const Details = () => {
+	const {name} = useParams();
+	const navigate = useNavigate();
+	const [country, setCountry] = useState(null);
+
+	useEffect(() => {
+		axios.get(searchByCountry(name)).then(({data}) => setCountry(data[0]))
+	}, [name])
 
 	return (
 		<div>
-			Details
+			<Button onClick={() => navigate(-1)}>
+				<IoArrowBack /> Back
+			</Button>
+			{country && <Info {...country} />}
 		</div>
 	);
 };
